e38c39f7e25b7a0e741b13d3f4da5541754be3f9
NotBaldrick <hello@mikenew.xyz>
2024-04-22 19:31:20 +0100

    add input youtube functionality

--- app/input/youtube/page.js
+++ app/input/youtube/page.js
@@ -1,32 +1,49 @@
+"use client";
+
 import BackButton from "@/components/back-button/back-button";
 import Button from "@/components/button/button";
+import createQuiz from "@/lib/createQuiz";
+import getYouTubeTranscript from "@/lib/getYouTubeTranscript";
+import { useEffect } from "react";
+import { useFormState } from "react-dom";
 
-// TODO: add functionality, currently only layout and styles are done.
 export default function InputYouTubePage() {
+	const [transcript, formAction] = useFormState(getYouTubeTranscript);
+
+	useEffect(() => {
+		if (!transcript) return;
+
+		createQuiz(transcript);
+	}, [transcript]);
+
 	return (
 		<>
-			<form className="mx-auto flex h-screen w-full flex-col items-center justify-center px-2 pb-8 sm:pb-16 lg:w-[40rem]">
+			<form
+				className="mx-auto flex h-screen w-full flex-col items-center justify-center px-2 pb-8 sm:pb-16 lg:w-[40rem]"
+				action={formAction}
+			>
 				<div className="flex w-full items-center justify-center sm:justify-between">
-					<h1 className="animate-fade-down sm:animate-fade-right text-2xl sm:pt-1 sm:text-4xl md:pt-2">
+					<h1 className="animate-fade-down text-2xl sm:animate-fade-right sm:pt-1 sm:text-4xl md:pt-2">
 						YouTube URL
 					</h1>
 
 					<Button
-						className="animate-fade-left animate-delay-200 hidden sm:block"
+						className="hidden animate-fade-left animate-delay-200 sm:block"
 						text="generate"
 						type="submit"
 					/>
 				</div>
 
 				<input
-					className="animate-fade sm:animate-fade-up animate-delay-200 sm:animate-delay-500 mt-2 h-14 w-full border-4 border-neutral-50 bg-transparent p-2 text-lg placeholder:uppercase focus:border-red-600 focus:outline-none sm:mt-4 sm:h-16 sm:text-3xl"
+					className="mt-2 h-14 w-full animate-fade border-4 border-neutral-50 bg-transparent p-2 text-lg animate-delay-200 placeholder:uppercase focus:border-red-600 focus:outline-none sm:mt-4 sm:h-16 sm:animate-fade-up sm:text-3xl sm:animate-delay-500"
 					placeholder="Input YouTube URL"
 					type="url"
+					name="youtube-url"
 					required
 				/>
 
 				<Button
-					className="animate-fade-up animate-delay-500 mt-4 block w-full sm:hidden"
+					className="mt-4 block w-full animate-fade-up animate-delay-500 sm:hidden"
 					text="generate"
 					type="submit"
 				/>
